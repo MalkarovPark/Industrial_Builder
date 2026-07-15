6d4800e5540ccde30fa71c6e3400020b10111fd8
MalkarovPark <62340924+MalkarovPark@users.noreply.github.com>
2026-07-15 15:40:26 +0300

    Provided autoscaling for part model view.

--- Industrial Builder/Utilites/ModuleDesigners/PartDesigner/PartModelView.swift
+++ Industrial Builder/Utilites/ModuleDesigners/PartDesigner/PartModelView.swift
@@ -56,17 +56,65 @@ struct PartModelView: View
     }
     #else
     @State private var scene_content: RealityViewContent?
+    @State private var model_size: SIMD3<Float> = .zero
+    @State private var view_size: CGSize = .zero
+    @State private var scale: Float = 1
+    let factor: Float = 0.5
+    let shift: Float = 200
     
     var body: some View
     {
-        RealityView
-        { content in
-            content.add(entity ?? Entity())
+        GeometryReader
+        { geometry in
+            RealityView
+            { content in
+                scene_content = content
+                
+                if preview_entity == nil
+                {
+                    preview_entity = (entity ?? Entity()).clone(recursive: true)
+                    
+                    if let preview_entity
+                    {
+                        let bounds = preview_entity.visualBounds(relativeTo: nil)
+                        model_size = bounds.extents
+                        
+                        content.add(preview_entity)
+                    }
+                }
+            }
+            .onChange(of: geometry.size)
+            { _, new_size in
+                view_size = new_size
+                update_scale()
+            }
+            .frame(depth: CGFloat(scale * model_size.x * 1000 + shift))
         }
         .onChange(of: entity)
         { old_value, new_value in
             update_entity(new_value)
         }
+    }
+    
+    private func update_scale()
+    {
+        guard let preview_entity else { return }
+        guard model_size != .zero else { return }
+
+        let viewWidth = Float(view_size.width) * 0.001
+        let viewHeight = Float(view_size.height) * 0.001
+
+        let minViewDimension = min(viewWidth, viewHeight)
+
+        let modelRadius = length(model_size) * 0.5
+
+        guard modelRadius > 0,
+              minViewDimension > 0
+        else { return }
+
+        scale = (minViewDimension / length(model_size)) * factor
+
+        preview_entity.scale = SIMD3<Float>(repeating: scale)
     }
     #endif
     
